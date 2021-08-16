7add72f8908c8e11a7b56cccdacaa809bb742bde
Jose Pereira <josepsneto@gmail.com>
2021-08-16 16:01:26 -0300

    Create model mapping

---
+++ src/main/java/com/zeneto/bookstoremanager/users/enums/Gender.java
@@ -0,0 +1,16 @@
+package com.zeneto.bookstoremanager.users.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum Gender {
+
+    MALE("Male"),
+    FEMALE("Female");
+
+    private String description;
+
+
+}
